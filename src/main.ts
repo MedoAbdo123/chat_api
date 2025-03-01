99755e7ab0a22c3ce8d3bf0dd2baefe6b837f644
Medo Abdo <Medoshakir590@gmail.com>
2025-03-02 01:27:23 +0400

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT);
+
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 
 bootstrap();
