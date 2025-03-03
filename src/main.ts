0d6a338f0645d19dde9a87b441eb20665712e4a8
Medo Abdo <Medoshakir590@gmail.com>
2025-03-03 21:20:48 +0400

    update vercel.json

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-
+  app.enableCors();
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 
-export default bootstrap();
+bootstrap();
